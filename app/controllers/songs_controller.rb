a663a25525b54da169512a1c3aa86a905697f988
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-01 17:48:49 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,3 +1,3 @@
-rails g resource Song title:string released:boolean release_year:integer artist_name:string genre:string
+# rails g resource Song title:string released:boolean release_year:integer artist_name:string genre:string
 class SongsController < ApplicationController
 end
