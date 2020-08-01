6d448621e1d7b424ab826dd034ba6a3cb7d9436b
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-01 17:48:40 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,4 @@
+# Songs have a title, a string, and an artist_name, a string 
+# rails g resource Song title:string artist_name:string --no-test-framework
 class SongsController < ApplicationController
 end
